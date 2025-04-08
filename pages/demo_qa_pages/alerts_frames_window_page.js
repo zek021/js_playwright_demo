887afbf6be896e6bce0e1871ffc7a4ebed034dca
zek021 <149951526+zek021@users.noreply.github.com>
2025-04-09 00:54:53 +0800

    added alerts handling

--- pages/demo_qa_pages/alerts_frames_window_page.js
+++ pages/demo_qa_pages/alerts_frames_window_page.js
@@ -9,6 +9,16 @@ export class AlertsFramesWindowsPage {
         this.browser_windows_new_tab_button = page.locator('#tabButton'); 
         this.browser_windows_new_window_button = page.locator('#windowButton'); 
         this.browser_windows_new_window_message_button = page.locator('#messageWindowButton'); 
+        // alerts
+        this.alert_frames_windows_alerts = page.locator('.left-pannel>.accordion>.element-group:nth-child(3)>div>.menu-list>#item-1'); 
+        this.alerts_alert_click_me = page.locator('#alertButton'); 
+        this.alerts_timed_alert_click_me = page.locator('#timerAlertButton'); 
+        this.alerts_confirmation_click_me = page.locator('#confirmButton'); 
+        this.alerts_confirmation_result_message = page.locator('#confirmResult'); 
+        this.alerts_prompt_click_me = page.locator('#promtButton'); 
+        this.alerts_prompt_result_message = page.locator('#promptResult');
+
+
        
     }
 
